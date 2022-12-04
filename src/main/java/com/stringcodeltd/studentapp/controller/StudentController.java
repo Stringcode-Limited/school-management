be2c8ee503e0bfeca1b9d43392b73856fa63a41f
Adebayo Michael <mcgamma04@yahoo.com>
2022-12-04 06:12:36 +0100

    refraced with service class

--- src/main/java/com/stringcodeltd/studentapp/controller/StudentController.java
+++ src/main/java/com/stringcodeltd/studentapp/controller/StudentController.java
@@ -4,7 +4,10 @@ import com.stringcodeltd.studentapp.dao.AddressRepository;
 import com.stringcodeltd.studentapp.dao.StudentRepository;
 import com.stringcodeltd.studentapp.model.Address;
 import com.stringcodeltd.studentapp.model.Student;
+import com.stringcodeltd.studentapp.service.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,68 +20,41 @@ import java.util.Optional;
 public class StudentController {
 
     @Autowired
-    private StudentRepository stdrepo;
+//    private StudentRepository stdrepo;
+    private StudentsService studentsService;
 
     @Autowired
     private AddressRepository addressrepo;
 
     @GetMapping()
-    public List<Student> getallStudent(){
-        return stdrepo.findAll();
+    public List<Student> getallStudent(){return studentsService.getAllStudentDetails();
     }
 //get student by matruc nomber
 
     @GetMapping("/{id}")
-    public Optional<Student> getStudentById(@PathVariable(name="id") Long stdid){
-        Optional<Student> byId = stdrepo.findById(stdid);
-
-        return byId.isPresent() ? Optional.of(byId.get()) : null;
+    public Optional<Student> getStudentById(@PathVariable(name="id") Long stdid){return studentsService.getAllStudentDetailsById(stdid);
 
     }
 
    @PostMapping()
-   public Student createStudent(@Valid @RequestBody Student student){
-        return stdrepo.save(student);
+   public Student createStudent(@Valid @RequestBody Student student){return studentsService.saveStudent(student);
    }
 
    @DeleteMapping("/{id}")
-   public String deleteStudent(@PathVariable(name="id") Long id){
-        if(stdrepo.existsById(id)) {
-            stdrepo.deleteById(id);
-            return "Student with id" + id + " deleted successfully";
-        }
-        return "There is no student with id "+id;
-
+   public String deleteStudent(@PathVariable(name="id") Long id){ return studentsService.deleteStudent(id);
    }
 
    @PutMapping("/{id}")
-   public String  updateStudent(@PathVariable(name="id")Long id, @Valid @RequestBody Student studentId){
-
-        if(stdrepo.existsById(id)){
-            Student std =  stdrepo.findById(id).get();
-            std.setFirstName(studentId.getFirstName());
-            std.setLastName(studentId.getLastName());
-            std.setDepartment(studentId.getDepartment());
-            std.setGender(studentId.getGender());
-            std.setLevel(studentId.getLevel());
-
-            stdrepo.save(std);
-            return "Record updated successfully";
-        }
-        return "No student with such an id";
+   public String  updateStudent(@PathVariable(name="id")Long id, @Valid @RequestBody Student studentId){ return  studentsService.updateStudent(id,studentId);
    }
 
 
-
-
 @GetMapping("/filterbydept/{department}")
-    public List<Student> getByDepartment(@PathVariable(name="department") String dept){
-        return stdrepo.findByDept(dept);
+    public List<Student> getByDepartment(@PathVariable(name="department") String dept){ return studentsService.getByDepartment(dept);
 }
 
 @GetMapping("/filterbygender/{gender}")
-    public List<Student> getByGender(@PathVariable(name="gender") String gender){
-        return stdrepo.findByGender(gender);
+    public List<Student> getByGender(@PathVariable(name="gender") String gender){ return studentsService.getByGender(gender);
 }
 
 
---
+++ src/main/java/com/stringcodeltd/studentapp/service/StudentsService.java
@@ -0,0 +1,62 @@
+package com.stringcodeltd.studentapp.service;
+
+import com.stringcodeltd.studentapp.dao.StudentRepository;
+import com.stringcodeltd.studentapp.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentsService {
+    @Autowired
+    private StudentRepository stdrepo;
+
+    public  Student saveStudent(Student student){
+        return stdrepo.save(student);
+    }
+
+    public List<Student> getAllStudentDetails(){
+     return stdrepo.findAll();
+    }
+
+    public Optional<Student> getAllStudentDetailsById(Long studentId){
+        Optional<Student> byId = stdrepo.findById(studentId);
+        return byId.isPresent() ? Optional.of(byId.get()) : null;
+    }
+
+    public String deleteStudent(Long id){
+        if(stdrepo.existsById(id)) {
+            stdrepo.deleteById(id);
+            return "Student with id" + id + " deleted successfully";
+        }
+        return "There is no student with id "+id;
+    }
+
+   public String updateStudent(Long id, Student studentId){
+       if(stdrepo.existsById(id)){
+           Student std =  stdrepo.findById(id).get();
+           std.setFirstName(studentId.getFirstName());
+           std.setLastName(studentId.getLastName());
+           std.setDepartment(studentId.getDepartment());
+           std.setGender(studentId.getGender());
+           std.setLevel(studentId.getLevel());
+
+           stdrepo.save(std);
+           return "Record updated successfully";
+       }
+       return "No student with such an id";
+   }
+
+    public List<Student> getByDepartment(String dept) {
+        return stdrepo.findByDept(dept);
+    }
+
+    public List<Student> getByGender(String gender){
+        return stdrepo.findByGender(gender);
+    }
+
+
+}
